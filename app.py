87fd578df15473ac238198d94c049edc1a2352bf
treysont1 <treysonthelusma1@gmail.com>
2025-12-18 21:11:22 -0500

    Add additional stock info categories

--- app.py
+++ app.py
@@ -17,10 +17,15 @@ db = SQLAlchemy(app)
 #Data - Row of Data
 class Stock(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ticker = db.Column(db.String(25), nullable=False)
-    initial = db.Column(db.Integer, default=0)
-    current = db.Column(db.Integer, default=0)
-    date_added = db.Column(db.DateTime, default=datetime.now)
+    ticker = db.Column(db.String(25), nullable=False) 
+    total_invested = db.Column(db.Integer, default=0) 
+    shares_owned = db.Column(db.Integer, default=0) 
+    avg_cost = db.Column(db.Integer, default=0) 
+    current_share_price = db.Column(db.Integer, default=0) 
+    total_value = db.Column(db.Integer, default=0) 
+    gain = db.Column(db.Integer, default=0) 
+    gain_percentage = db.Column(db.Integer, default=0) 
+    date_bought = db.Column(db.DateTime, default=datetime.now)
     def __repr__(self):
         return f"Stock {self.id}"
 
@@ -42,8 +47,15 @@ def index():
             return f"Error:{e}"
     # See portfolio
     else:
-        portfolio = Stock.query.order_by(Stock.date_added).all()
+        portfolio = Stock.query.order_by(Stock.date_bought).all()
         return render_template("index.html", portfolio=portfolio)
+
+#Stock Info
+@app.route("/info/<int:id>", methods=["GET"])
+def view():
+    stock_view = Stock.query.get_or_404(id)
+    return render_template('info.html', stock=stock_view)
+
 
 #Remove Stock
 @app.route("/delete/<int:id>", methods=["POST", "GET"])
@@ -64,11 +76,12 @@ def update(id):
     stock_update = Stock.query.get_or_404(id)
     if request.method == "POST":
         stock_update.ticker = request.form['stock_ticker']
-        stock_update.initial = request.form['stock_initial']
-        stock_update.current = request.form['stock_current']
+        stock_update.total_invested = request.form['stock_total_invested']
+        stock_update.shares_owned = request.form['stock_shares']
+        stock_update.current_share_price = request.form['stock_current_share_price']
         # print("HERE IS DATETIME")
-        # print(type(request.form['date_added']))
-        # stock_update.date_added = datetime(request.form['date_added'])
+        # print(type(request.form['date_bought']))
+        # stock_update.date_bought = datetime(request.form['date_bought'])
         try:
             db.session.commit()
             return redirect("/")
