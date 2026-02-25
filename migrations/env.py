f9e9894bc53c41e0c404b76dc8e8178d03fd248f
treysont1 <treysonthelusma1@gmail.com>
2026-02-25 00:16:09 -0500

    Update migrations

--- migrations/env.py
+++ migrations/env.py
@@ -5,10 +5,6 @@ from flask import current_app
 
 from alembic import context
 
-import os
-
-from dotenv import load_dotenv
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -54,12 +50,6 @@ def get_metadata():
         return target_db.metadatas[None]
     return target_db.metadata
 
-load_dotenv(dotenv_path="env/.env")
-
-database_url = os.getenv('SQLALCHEMY_DATABASE_URI')
-
-if database_url:
-    config.set_main_option("sqlalchemy.url", database_url)
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
