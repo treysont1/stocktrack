be1d3507176916221cf87503875e30c622d2750d
treysont1 <treysonthelusma1@gmail.com>
2026-03-20 19:39:03 -0400

    Handle invalid api calls

--- stock_validation.py
+++ stock_validation.py
@@ -4,13 +4,17 @@ def validate_ticker(ticker):
     url = f"https://stockprices.dev/api/stocks/{ticker}"
     results = requests.get(url)
     if results.status_code == 200:
-        return True
+        return (True)
     elif results.status_code == 404:
-        return False
-    return None
+        return (False, "Invalid Ticker.")
+    return (False, "API ISSUE")
 
 def get_current_price(ticker):
+    # print("called")
     url = f"https://stockprices.dev/api/stocks/{ticker}"
     results = requests.get(url)
+    if results.status_code != 200:
+        return None
     stock_data = results.json()
+    # print(type(stock_data), stock_data)
     return stock_data['Price']
