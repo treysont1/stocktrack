4529b2529c985454dd3b8f7fc5179ae0c6b09fc0
treysont1 <treysonthelusma1@gmail.com>
2026-03-20 20:12:20 -0400

    Add placeholder values for api (currently switching api)

--- stock_validation.py
+++ stock_validation.py
@@ -1,20 +1,22 @@
 import requests
 
 def validate_ticker(ticker):
-    url = f"https://stockprices.dev/api/stocks/{ticker}"
-    results = requests.get(url)
-    if results.status_code == 200:
-        return (True)
-    elif results.status_code == 404:
-        return (False, "Invalid Ticker.")
-    return (False, "API ISSUE")
+    return (True, "_")
+    # url = f"https://stockprices.dev/api/stocks/{ticker}"
+    # results = requests.get(url)
+    # if results.status_code == 200:
+    #     return (True, "_")
+    # elif results.status_code == 404:
+    #     return (False, "Invalid Ticker.")
+    # return (False, "API ISSUE")
 
 def get_current_price(ticker):
-    # print("called")
-    url = f"https://stockprices.dev/api/stocks/{ticker}"
-    results = requests.get(url)
-    if results.status_code != 200:
-        return None
-    stock_data = results.json()
-    # print(type(stock_data), stock_data)
-    return stock_data['Price']
+    return 14
+    # # print("called")
+    # url = f"https://stockprices.dev/api/stocks/{ticker}"
+    # results = requests.get(url)
+    # if results.status_code != 200:
+    #     return None
+    # stock_data = results.json()
+    # # print(type(stock_data), stock_data)
+    # return stock_data['Price']
