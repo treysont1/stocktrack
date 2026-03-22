dc01420dfa331fb608de589e87f78bf3926b54cb
treysont1 <treysonthelusma1@gmail.com>
2026-03-22 03:02:49 -0400

    Create holding, move functions out of model to service.py

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(128), unique=True)
     password_hash = db.Column(db.String(128))
-    stocks: so.Mapped[list['Stock']] = db.relationship("Stock", back_populates='user', cascade='all, delete-orphan')
+    holdings: so.Mapped[list['Holding']] = db.relationship("Holding", back_populates='user', cascade='all, delete-orphan')
 
     @property
     def password(self):
@@ -29,43 +29,26 @@ class User(UserMixin, db.Model):
     
     def __repr__(self):
         return f"User {self.username}"
-
-class Stock(db.Model):
+    
+class Holding(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ticker = db.Column(db.String(25), nullable=False) 
-    total_invested = db.Column(db.Integer, default=0) 
-    bought_at = db.Column(db.Integer, default=0)
-    shares_owned = db.Column(db.Integer, default=0) 
-    avg_cost = db.Column(db.Integer, default=0) 
-    current_share_price = db.Column(db.Integer, default=0) 
-    total_value = db.Column(db.Integer, default=0) 
-    gain = db.Column(db.Integer, default=0) 
-    gain_percentage = db.Column(db.Integer, default=0) 
-    date_bought = db.Column(db.DateTime, default=datetime.now)
-    user_id: so.Mapped[int] = db.Column(db.Integer, db.ForeignKey("user.id", ondelete='CASCADE'), nullable=False, index=True)
-    user: so.Mapped[User] = db.relationship("User", back_populates='stocks')
-    transactions: so.Mapped[list['Transaction']] = db.relationship("Transaction", back_populates='stock', cascade='all, delete-orphan', order_by='Transaction.time')
 
-    @cached_property
-    def current_price(self):
-        return get_current_price(self.ticker)
-    @property
-    def shares_owned(self):
-        remaining_shares = self._calculate_fifo()
-        return sum(shares for shares, _ in remaining_shares)
-    @property
-    def total_invested(self):
-        remaining_shares = self._calculate_fifo()
-        return sum(shares * price for shares, price in remaining_shares)
-    @property
-    def average_cost(self):
-        if self.shares_owned == 0:
-            return 0
-        return self.total_invested / self.shares_owned
+    user_id: so.Mapped[int] = db.Column(db.Integer, db.ForeignKey("user.id", ondelete='CASCADE'), nullable=False, index=True)
+    user: so.Mapped[User] = db.relationship("User", back_populates='holdings')
+
+    stock_id: so.Mapped[int] = db.Column(db.Integer, db.ForeignKey("stock.id", ondelete='CASCADE'), nullable=False, index=True)
+    stock: so.Mapped['Stock'] = db.relationship("Stock", back_populates='holding')
+
+    shares_owned = db.Column(db.Float, default=0)
+    average_price = db.Column(db.Float, default=0)
+    total_invested = db.Column(db.Float, default=0)
+
+    transactions: so.Mapped[list['Transaction']] = db.relationship("Transaction", back_populates='holding', cascade='all, delete-orphan', order_by='Transaction.time')
+
     @property
     def current_value(self):
         if self.current_price is not None:
-            return self.shares_owned * self.current_price
+            return self.shares_owned * self.stock.current_price
         return None
     @property
     def unrealized_gain(self):
@@ -78,23 +61,13 @@ class Stock(db.Model):
             return 100 * self.unrealized_gain / self.total_invested
         return None
 
-
-    def _calculate_fifo(self):
-        remaining_shares = []
-        for t in self.transactions:
-            if t.type == "BUY":
-                remaining_shares.append([t.shares, t.price_per_share])
-            else:
-                shares_to_sell = t.shares
-                while shares_to_sell > 0 and remaining_shares:
-                    first_in = remaining_shares[0]
-                    if first_in[0] > shares_to_sell:
-                        first_in[0] -= shares_to_sell
-                        shares_to_sell = 0
-                    else:
-                        shares_to_sell -= first_in[0]
-                        remaining_shares.pop(0)
-        return remaining_shares
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ticker = db.Column(db.String(25), nullable=False) 
+    current_price = db.Column(db.Float)
+    total_value = db.Column(db.Integer, default=0) 
+    last_updated = db.Column(db.DateTime)
+    holdings: so.Mapped[list['Holding']] = db.relationship("Holding", back_populates='stock', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f"Stock {self.id}: {self.ticker}"
@@ -105,8 +78,8 @@ class Transaction(db.Model):
     shares = db.Column(db.Float, nullable=False)
     price_per_share = db.Column(db.Float, nullable=False)
     time = db.Column(db.DateTime, default=datetime.now)
-    stock_id: so.Mapped[int] = db.Column(db.Integer, db.ForeignKey("stock.id", ondelete='CASCADE'), nullable=False, index=True)
-    stock: so.Mapped['Stock'] = db.relationship("Stock", back_populates='transactions')
+    holding_id: so.Mapped[int] = db.Column(db.Integer, db.ForeignKey("holding.id", ondelete='CASCADE'), nullable=False, index=True)
+    holding: so.Mapped['Holding'] = db.relationship("Holding", back_populates='transactions')
    
 class StockHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
---
+++ service.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+#Important Functions 
+
+
+def calculate_fifo(holding):
+        lots = []
+        for t in holding.transactions:
+            if t.type == "BUY":
+                lots.append((t.shares, t.price_per_share))
+            else:
+                shares_to_sell = t.shares
+                while shares_to_sell > 0 and lots:
+                    lots = lots[0]
+                    if lots[0][0] > shares_to_sell:
+                        lots[0][0] -= shares_to_sell
+                        shares_to_sell = 0
+                    else:
+                        shares_to_sell -= lots[0][0]
+                        lots.pop(0)
+
+        holding.shares_owned = sum(lot[0] for lot in lots)
+        if holding.shares_owned > 0:
+            total_cost = sum(lot[0] * lot[1] for lot in lots)
+            holding.average_price = total_cost / holding.shares
+        else:
+            holding.average_price = 0
+
+# threshold will be datetime.timedelta(days=1)
+def is_stale(stock, threshold):
+    if stock.last_update.date() + threshold > datetime.now():
+        return True
+    return False
