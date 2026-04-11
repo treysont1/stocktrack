5fcbb7b5a9975887706e3444a97f5155720ab46d
treysont1 <treysonthelusma1@gmail.com>
2026-04-10 22:45:15 -0400

    Fix hash column length

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(128), unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     holdings: so.Mapped[list['Holding']] = db.relationship("Holding", back_populates='user', cascade='all, delete-orphan')
     # portfolio_history: so.Mapped['PortfolioHistory'] = db.relationship("PortfolioHistory", back_populates='user')
 
